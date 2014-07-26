63328b38cda966719cf014ee6f68085d0665d504
Wesley W. Terpstra <wesley@terpstra.ca>
2014-07-27 00:33:21 +0200

    dpram: "old data" style for faster synthesis

--- opa_dpram.vhd
+++ opa_dpram.vhd
@@ -25,21 +25,18 @@ end opa_dpram;
 architecture rtl of opa_dpram is
   type t_memory is array(g_size-1 downto 0) of std_logic_vector(g_width-1 downto 0);
   signal r_memory : t_memory;
-  
-  signal r_addr : std_logic_vector(r_addr_i'range);
 begin
 
-  -- !!! bad ==> implies "new data" mode. want "dont care".
-  r_data_o <= r_memory(to_integer(unsigned(r_addr)));
-  
   main : process(clk_i) is
   begin
     if rising_edge(clk_i) then
-      r_addr <= r_addr_i;
-      
       if w_en_i = '1' then
         r_memory(to_integer(unsigned(w_addr_i))) <= w_data_i;
       end if;
+      
+      -- don't be fooled: after inference this is not a register!
+      -- r_addr_i will be the register and r_data_o has latency after the clock
+      r_data_o <= r_memory(to_integer(unsigned(r_addr_i)));
     end if;
   end process;
 
