fefaf9cf5d8702474b7fc06e7236a930018c4c96
Wesley W. Terpstra <wesley@terpstra.ca>
2014-08-01 19:06:16 +0200

    tb: support "programs"

--- opa_core_tb.vhd
+++ opa_core_tb.vhd
@@ -14,7 +14,42 @@ end opa_core_tb;
 
 architecture rtl of opa_core_tb is
 
+  type t_ops is array(15 downto 0) of std_logic_vector(67 downto 0);
+  constant c_op : t_ops := 
+    (0      => x"12000200020002000",
+     1      => x"12000200020002000",
+     2      => x"12000200020002000",
+     3      => x"12000200020002000",
+     4      => x"12000200020002000",
+     5      => x"12000200020002000",
+     6      => x"12000200020002000",
+     7      => x"12000200020002000",
+     8      => x"12000200020002000",
+     9      => x"12000200020002000",
+     10     => x"12000200020002000",
+     others => x"02000200020002000");
+
+  signal r_off : unsigned(3 downto 0);
+  
+  signal s_stall : std_logic;
+  signal s_stb   : std_logic;
+  signal s_op    : std_logic_vector(63 downto 0);
+  
 begin
+
+  test : process(clk_i, rstn_i) is
+  begin
+    if rstn_i = '0' then
+      r_off <= (others => '0');
+    elsif rising_edge(clk_i) then
+      if s_stall = '0' then
+        r_off <= r_off + 1;
+      end if;
+    end if;
+  end process;
+  
+  s_stb <= c_op(to_integer(r_off))(64);
+  s_op  <= c_op(to_integer(r_off))(63 downto 0);
 
   opa_core : opa
     generic map(
@@ -22,9 +57,9 @@ begin
     port map(
       clk_i   => clk_i,
       rst_n_i => rstn_i,
-      stb_i   => '1',
-      stall_o => open,
-      data_i  => x"1201132114211400");
+      stb_i   => s_stb,
+      stall_o => s_stall,
+      data_i  => s_op);
 
   good_o <= '1';
 
