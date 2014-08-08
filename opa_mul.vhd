5b43395e05df809762fbc15975c136eff7fe39e7
Wesley W. Terpstra <wesley@terpstra.ca>
2014-08-08 19:55:39 +0200

    mul: pipeline it and add high/low mux

--- opa_mul.vhd
+++ opa_mul.vhd
@@ -29,26 +29,39 @@ architecture rtl of opa_mul is
   signal r_data : std_logic_vector(reg_data_i'range);
   signal r_datb : std_logic_vector(reg_datb_i'range);
   signal r_datx : std_logic_vector(reg_data_i'length*2-1 downto 0);
+  signal r_daty : std_logic_vector(reg_data_i'length*2-1 downto 0);
+  signal r_auxw : std_logic;
+  signal r_auxx : std_logic;
+  signal r_auxy : std_logic;
   signal r_regx : std_logic_vector(iss_regx_i'range);
   signal r_regy : std_logic_vector(iss_regx_i'range);
+  signal r_regz : std_logic_vector(iss_regx_i'range);
 
 begin
 
-  iss_regx_o <= r_regx; -- latency=2
+  iss_regx_o <= r_regy; -- latency=3
   
   main : process(clk_i) is
   begin
     if rising_edge(clk_i) then
       r_data <= reg_data_i;
       r_datb <= reg_datb_i;
-      r_regx <= iss_regx_i;
-      r_regy <= r_regx;
+      r_auxw <= aux_dat_i(0);
       
-      reg_regx_o <= r_regy;
       r_datx <= std_logic_vector(unsigned(r_data) * unsigned(r_datb));
+      r_auxx <= r_auxw;
+      r_daty <= r_datx;
+      r_auxy <= r_auxx;
+      
+      r_regx  <= iss_regx_i;
+      r_regy  <= r_regx;
+      r_regz  <= r_regy;
+      
+      reg_regx_o <= r_regz;
     end if;
   end process;
   
-  reg_datx_o <= r_datx(reg_datx_o'range);
+  reg_datx_o <= r_daty(reg_datx_o'range) when r_auxy='0' else
+                r_daty(reg_data_i'length*2-1 downto reg_data_i'length);
 
 end rtl;
