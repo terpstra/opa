9d75ab1133f1fa3e0aa04040568b0ae812de8ae5
Wesley W. Terpstra <wesley@terpstra.ca>
2014-08-19 18:05:39 +0200

    syn: tweaked testbench to remove bypassed M9K => was slowing down design

--- opa_syn_tb.vhd
+++ opa_syn_tb.vhd
@@ -22,7 +22,7 @@ architecture rtl of opa_syn_tb is
   constant c_config : t_opa_config := c_opa_mid;
 
   type t_ops is array(15 downto 0) of std_logic_vector(67 downto 0);
-  signal r_op : t_ops :=
+  signal r_ops : t_ops :=
     (0      => x"02000200020002000",
      1      => x"12111211121112111",
      2      => x"12666266626662666",
@@ -36,6 +36,7 @@ architecture rtl of opa_syn_tb is
      others => x"02000200020002000");
 
   signal r_off   : unsigned(3 downto 0);
+  signal s_off   : unsigned(3 downto 0);
   
   signal r_we    : std_logic;
   signal r_addr  : std_logic_vector( 3 downto 0);
@@ -45,9 +46,8 @@ architecture rtl of opa_syn_tb is
   signal r_out   : std_logic;
   
   signal s_stall : std_logic;
-  signal s_stb   : std_logic;
-  signal s_op    : std_logic_vector(c_config.num_decode*16-1 downto 0);
-  
+  signal r_stb   : std_logic;
+  signal r_op    : std_logic_vector(c_config.num_decode*16-1 downto 0);
   
 begin
 
@@ -59,10 +59,11 @@ begin
     elsif rising_edge(clk_i) then
       r_out <= s_out;
       if s_stall = '0' then
-        r_off <= r_off + 1;
+        r_off <= s_off;
       end if;
     end if;
   end process;
+  s_off <= r_off + 1;
   
   ram : process(clk_i) is
   begin
@@ -71,13 +72,12 @@ begin
       r_addr <= addr_i;
       r_data <= data_i;
       if r_we = '1' then
-        r_op(to_integer(unsigned(r_addr)))(r_data'range) <= r_data;
+        r_ops(to_integer(unsigned(r_addr)))(r_data'range) <= r_data;
       end if;
+      r_stb <= r_ops(to_integer(s_off))(64);
+      r_op  <= r_ops(to_integer(s_off))(r_op'range);
     end if;
   end process;
-  
-  s_stb <= r_op(to_integer(r_off))(64);
-  s_op  <= r_op(to_integer(r_off))(s_op'range);
 
   opa_core : opa
     generic map(
@@ -86,9 +86,9 @@ begin
     port map(
       clk_i   => clk_i,
       rst_n_i => rstn_i,
-      stb_i   => s_stb,
+      stb_i   => r_stb,
       stall_o => s_stall,
-      data_i  => s_op,
+      data_i  => r_op,
       good_o  => s_out);
 
   good_o <= r_out;
