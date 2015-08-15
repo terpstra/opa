8c9f3358bb3b977ecbde5461a30f4465ad821860
Wesley W. Terpstra <wesley@terpstra.ca>
2015-08-15 16:49:30 +0200

    gtkwave: works!

---
+++ ghdl/sim.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+set -e
+for i in \
+	opa_pkg.vhd 		\
+	opa_functions_pkg.vhd	\
+	opa_components_pkg.vhd	\
+	opa_commit.vhd		\
+	opa_core_tb.vhd		\
+	opa_dbus.vhd		\
+	opa_decode.vhd		\
+	opa_dpram.vhd		\
+	opa_fast.vhd		\
+	opa_issue.vhd		\
+	opa_l1d.vhd		\
+	opa_lcell.vhd		\
+	opa_ldst.vhd		\
+	opa_lsb.vhd		\
+	opa_prefixsum.vhd	\
+	opa_prim_mul.vhd	\
+	opa_prim_ternary.vhd	\
+	opa_regfile.vhd		\
+	opa_rename.vhd		\
+	opa_sim_tb.vhd		\
+	opa_slow.vhd		\
+	opa.vhd;		\
+do ghdl -a --std=02 --ieee=synopsys ../$i
+done
+
+ghdl -e --std=02 --ieee=synopsys opa_sim_tb
+./opa_sim_tb --stop-time=500ns --vcd=testbench.vcd
+gtkwave testbench.vcd wave.gtkw
