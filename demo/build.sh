03f2b95a1d23dc52bd0430d000a0f379ae0efa98
Wesley W. Terpstra <wesley@terpstra.ca>
2015-08-26 00:18:32 +0200

    a valid demo program

--- demo/build.sh
+++ demo/build.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
+set -ex
 riscv-gcc -Wall -O2 -m32 crt0.S main.c -nostdlib -T ram.ld -o demo.elf
 riscv-objcopy -O binary demo.elf demo.bin
 gcc -Wall -O2 genramvhd.c -o genramvhd
-./genramvhd  demo.bin > demo.vhd
+./genramvhd -l -w 8 demo.bin > demo.vhd # -w 8 for 64-bit, -w 4 for 32-bit
