8cbea910b9d97ef91794bd03babe59c5820bb3b0
Wesley W. Terpstra <wesley@terpstra.ca>
2015-09-03 02:30:33 +0200

    demo: opa runs faster when loops are aligned to 16 bytes (a cache line)

--- demo/build.sh
+++ demo/build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -ex
-riscv64-unknown-elf-gcc -Wall -O2 -m32 crt0.S main.c -nostdlib -T ram.ld -o demo.elf
+riscv64-unknown-elf-gcc -falign-loops=16 -falign-functions=16 -Wall -O2 -m32 crt0.S main.c -nostdlib -T ram.ld -o demo.elf
 riscv64-unknown-elf-objcopy -O binary demo.elf demo.bin
 gcc -Wall -O2 genramvhd.c -o genramvhd
-./genramvhd -l -w 8 demo.bin > demo.vhd # -w 8 for 64-bit, -w 4 for 32-bit
+./genramvhd -l -w 8 -s 256 demo.bin > demo.vhd # -w 8 for 64-bit, -w 4 for 32-bit
