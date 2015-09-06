5c57610e896f686e38c89e2d66e2d7f44d6e67f7
Wesley W. Terpstra <wesley@terpstra.ca>
2015-09-07 01:46:04 +0200

    demo: include a program using (un)signed byte/short loading
    NOTE: lm32 will fail to run this program as the compiler incorrectly
    inserts "00000000" into alignment gaps! this means "sri r0, r0, 0" and
    as my shifter currently outputs "XXX", this breaks r0.

--- demo/main.c
+++ demo/main.c
@@ -1,13 +1,17 @@
-int buf[16];
+short buf1[16] = { 0, -1,  2, -3,  4, -5,  6, -7,  8, -9,  10, -11, 12, -13,  14, -15 };
+char  buf2[16] = { 0,  1, -2, -3,  4,  5, -6, -7,  8,  9, -10, -11, 12,  13, -14, -15 };
 const char name[] = "hello world";
 
 int main() {
-  int i;
-  int x = 6521;
-  int y = 8991;
-  for (i = 0; i < 16; ++i) {
-    x *= y;
-    buf[i] = x;
+  int sum = 0;
+  int i, j;
+  
+  for (j = 0; j < 2; ++j) {
+    // first iteration should be full of cache misses. second should be a rocket.
+    for (i = 0; i < 16; ++i) {
+      sum += buf1[i] * buf2[i];
+    }
   }
-  return x;
+  
+  return sum;
 }
