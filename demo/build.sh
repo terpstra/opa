c492770d8838e8dca2466e6a99310c046023a7f6
Wesley W. Terpstra <wesley@terpstra.ca>
2015-09-09 22:32:37 +0200

    sim: start simulating a 32-bit processor instead of 64
    RISC-V and LM32 are both actually 32-bit ISAs, and they might notice
    that registers don't overflow where expected in a more complicated
    program. So just simulate 32-bit.

--- demo/build.sh
+++ demo/build.sh
@@ -9,5 +9,5 @@ lm32-elf-gcc -mmultiply-enabled -mbarrel-shift-enabled -mno-sign-extend-enabled 
 lm32-elf-objcopy -O binary lm32.elf lm32.bin
 
 gcc -Wall -O2 genramvhd.c -o genramvhd
-./genramvhd -p demo -l -w 8 -s 512 riscv.bin > riscv.vhd # -w 8 for 64-bit, -w 4 for 32-bit
-./genramvhd -p demo -b -w 8 -s 512 lm32.bin  > lm32.vhd
+./genramvhd -p demo -l -w 4 -s 512 riscv.bin > riscv.vhd # -w 8 for 64-bit, -w 4 for 32-bit
+./genramvhd -p demo -b -w 4 -s 512 lm32.bin  > lm32.vhd
