7161f88e387323a0f7774759fe6da53dc82bd964
Wesley W. Terpstra <wesley@terpstra.ca>
2015-09-19 14:58:26 +0200

    demo: include a nastier test program

--- demo/main.c
+++ demo/main.c
@@ -1,6 +1,7 @@
 short buf1[16] = { 0, -1,  2, -3,  4, -5,  6, -7,  8, -9,  10, -11, 12, -13,  14, -15 };
 char  buf2[16] = { 0,  1, -2, -3,  4,  5, -6, -7,  8,  9, -10, -11, 12,  13, -14, -15 };
 const char name[] = "hello world";
+char zeros[200];
 
 int main() {
   int sum = 0;
@@ -9,7 +10,8 @@ int main() {
   for (j = 0; j < 2; ++j) {
     // first iteration should be full of cache misses. second should be a rocket.
     for (i = 0; i < 16; ++i) {
-      sum += buf1[i] * buf2[i];
+      sum += buf1[i] * buf2[i] + (buf2[i] << i);
+      buf1[i] = sum;
     }
   }
   
