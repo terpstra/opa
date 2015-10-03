59b3f11297008c9f6e7a356b290ad952ef80610f
Wesley W. Terpstra <wesley@terpstra.ca>
2015-10-03 17:06:58 +0200

    demo: add hello world

--- demo/main.c
+++ demo/main.c
@@ -1,11 +1,16 @@
 short buf1[16] = { 0, -1,  2, -3,  4, -5,  6, -7,  8, -9,  10, -11, 12, -13,  14, -15 };
 char  buf2[16] = { 0,  1, -2, -3,  4,  5, -6, -7,  8,  9, -10, -11, 12,  13, -14, -15 };
-const char name[] = "hello world";
+const char hello[] = "hello world\n";
 char zeros[200];
+
+volatile unsigned int* stdout = (unsigned int*)0x80000000U;
 
 int main() {
   int sum = 0;
   int i, j;
+  
+  for (i = 0; i < sizeof(hello); ++i)
+    *stdout = hello[i];
   
   for (j = 0; j < 2; ++j) {
     // first iteration should be full of cache misses. second should be a rocket.
